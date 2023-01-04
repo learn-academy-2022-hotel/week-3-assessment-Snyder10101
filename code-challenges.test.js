01de6a3773d914ec47c160392a65b169fe39590b
Snyder10101 <christiansnyder562@gmail.com>
2023-01-04 08:40:45 -0800

    completed week-3-assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -12,56 +12,128 @@
 
 // --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
 
-// a) Create a test with expect statements for each of the variables provided.
-
+// // a) Create a test with expect statements for each of the variables provided.
 const fibLength1 = 6
 // Expected output: [1, 1, 2, 3, 5, 8]
 
 const fibLength2 = 10
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+// TEST 
+describe ("fibSeq", () => {
+  it ("takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence ", () =>{
+    expect(fibSeq(fibLength1)).toEqual([1, 1, 2, 3, 5, 8])
+    expect(fibSeq(fibLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+  }) 
+})
+// Ran yarn jest
+// ReferenceError: fibSeq is not defined (good error)
 
 // b) Create the function that makes the test pass.
+
+const fibSeq = (array) => {
+  let fib = []
+  // Initialize array!
+  // number sequences
+  let zero = 0
+  let one = 1
+  // Next fibonacci number = previous + one before previous
+ for ( let i = 0;  i <array+1; i++){
+  // create a loop 
+   let bothNum = zero + one;
+  fib.push(zero);
+  zero = one;
+  one = bothNum;
+ }
+ fib.shift()
+  return fib
+}
 
 // --------------------2) Create a function that takes in an object and returns an array of the values sorted from least to greatest.
 // Hint: Check out this resource: Object.values() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_objects/Object/values
 
 // a) Create a test with expect statements for each of the variables provided.
+describe("inOrder", () => {
+  it("takes in an object and returns an array of the values sorted from least to greatest", () => {
+    const studyMinutesWeek1 = {
+      sunday: 90,
+      monday: 30,
+      tuesday: 20,
+      wednesday: 15,
+      thursday: 30,
+      friday: 15,
+      saturday: 60
+    }
+    const studyMinutesWeek2 = {
+      sunday: 100,
+      monday: 10,
+      tuesday: 45,
+      wednesday: 60,
+      thursday: 20,
+      friday: 15,
+      saturday: 65
+    }
+    expect (inOrder(studyMinutesWeek1)).toEqual([15, 15, 20, 30, 30, 60, 90])
+    expect (inOrder(studyMinutesWeek2)).toEqual([10, 15, 20, 45, 60, 65, 100])
+  })
+  // Expected output: [15, 15, 20, 30, 30, 60, 90]
+  // Expected output: [10, 15, 20, 45, 60, 65, 100]
+})
+// console.log(Object.values(studyMinutes))
+// ReferenceError: inOrder is not defined
+// good error
 
-const studyMinutesWeek1 = {
-  sunday: 90,
-  monday: 30,
-  tuesday: 20,
-  wednesday: 15,
-  thursday: 30,
-  friday: 15,
-  saturday: 60
-}
-// Expected output: [15, 15, 20, 30, 30, 60, 90]
 
-const studyMinutesWeek2 = {
-  sunday: 100,
-  monday: 10,
-  tuesday: 45,
-  wednesday: 60,
-  thursday: 20,
-  friday: 15,
-  saturday: 65
-}
-// Expected output: [10, 15, 20, 45, 60, 65, 100]
 
 // b) Create the function that makes the test pass.
+let inOrder = (obj) => {
+  // create a variable with object.value to return an array with the objects values
+  let arrayValue = Object.values(obj)
+  // I make a method by basically says hey obj make arrayValue the manipulation of values 
+  arrayValue.push(obj[arrayValue])
+  //return the values in order of least to greatest
+  return arrayValue.sort((a, b) => a - b)
+  }
+
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
-
 const accountTransactions1 = [100, -17, -23, -9]
 // Expected output: [100, 83, 60, 51]
-
 const accountTransactions2 = [250, -89, 100, -96]
 // Expected output: [250, 161, 261, 165]
-
 const accountTransactions3 = []
 // Expected output: []
+describe("accSum", () => {
+ it("takes in an array and returns an array of the accumulating sum", () => {
+expect (accSum(accountTransactions1)).toEqual([100, 83, 60, 51])
+expect (accSum(accountTransactions2)).toEqual([250, 161, 261, 165])
+expect (accSum(accountTransactions3)).toEqual([])
+ })
+ // ReferenceError: accSum is not defined good error
+})
+
 
 // b) Create the function that makes the test pass.
+// accSum is the fuction
+const accSum = (array) => {
+
+let val = array[0] 
+
+let sumArray = []
+// makeshift array
+
+for (let i=0; i < array.length; i++) {
+   if (array === []) {
+  return array
+//returning empty array
+ } else if (i === 0) {
+  sumArray.push(val)
+// console.log(sumArray.push(val))
+ } else {
+  sumArray.push(array[i] + sumArray[i-1])
+ }
+ //pushing the sum into the array
+}
+return sumArray
+}
--- interview-questions.md
+++ interview-questions.md
@@ -8,48 +8,48 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
-Your answer:
+Your answer:- JSX is an application that allows html in javascript the syntax difference is fragments holding the code
 
-Researched answer:
+Researched answer:- JSX lets you write HTML in javascript conviently without having to ref html and java together overall just easier to write and update applications,the one syntax "fragments" is the differenece between complied JSX and normal html.
 
 2. What is yarn? What file(s) are modified in a React application when you run the command yarn in your terminal?
 
-Your answer:
+Your answer:- “Yet Another Resource Negotiator“ also known as yarn , not to sure what this question meant.
 
-Researched answer:
+Researched answer:- Yarn uses are functionalities of resource management and job scheduling/monitoring as well as a tools to interact with applications,yarn.lock
 
-3. What is a React component?
+3. What is a React component? React components are usable code in react that kinda act like javascript fuctions.
 
-Your answer:
+Your answer:- React components are usable code in react that kinda act like javascript fuctions.
 
-Researched answer:
+Researched answer:- React components are independent and reusable code in react that kinda do act like javascript fuctions and they're two types (Class components, function components)
 
 4. What is the difference between state values and props in React?
 
-Your answer:
+Your answer:- props get passed to the component,state is managed within the component 
 
-Researched answer:
+Researched answer:- state are variables and is managed within the component, props get passed through component properties like fuctional parameters
 
 5. What is the DOM?
 
-Your answer:
+Your answer: -DOM is a user interface you can interact with, in an application 
 
-Researched answer:
+Researched answer: -DOM short for (Document Object Model) is an interface that you can interact with via applocation that gives an output that relates to your website.
 
 6. STRETCH: Which is the difference between a div and a span?
 
-Your answer:
+Your answer:- div is just a html element to groups sections of code together, not sure 
 
-Researched answer:
+Researched answer:- Span and div are both HTML elements that group together related parts of a application although, they serve different functions being span is used for organization and styling and div is used for code organization 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Object-oriented programming:
+1. Object-oriented programming: -Object-oriented programming is a style of programming characterized by the id  of a classes of objects closely related with the methods functions with which they are associated. It also includes ideas of inheritance of attributes and methods.
 
-2. Ruby:
+2. Ruby:- Ruby is an open source, interpreted, object-oriented programming language
 
-3. Implicit return:
+3. Implicit return:- Implicit Return is a function is returned values without using the return keyword
 
-4. Ruby blocks:
+4. Ruby blocks: Ruby blocks are little anonymous functions that can be passed into methods.
 
-5. Ruby hashes:
+5. Ruby hashes:- In Ruby, Hash is a collection of unique keys and their values. Hash is like an Array, except the indexing is done with the help of arbitrary keys of any object type
